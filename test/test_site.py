1429f0ff8f209a55fe26c0b41a3691fbbb09791c
Katya <kotagomon@gmail.com>
2026-06-24 16:43:25 -0700

    tests ready

---
+++ test/test_site.py
@@ -0,0 +1,34 @@
+import time
+from pages.homepage import HomePage
+from pages.product import ProductPage
+
+
+
+def test_open_s6(driver):
+    homepage = HomePage(driver)
+    homepage.open()
+    homepage.click_galaxy_s6()
+    product_page = ProductPage(driver)
+    product_page.check_title_is('Samsung galaxy s6')
+
+    # driver.get('https://demoblaze.com/index.html')
+    # galaxy_s6 = driver.find_element(By.XPATH, "//a[text()='Samsung galaxy s6']")
+    # galaxy_s6.click()
+    # title = driver.find_element(By.CSS_SELECTOR, 'h2')
+    # assert title.text == 'Samsung galaxy s6'
+
+def test_two_monitors(driver):
+    homepage = HomePage(driver)
+    homepage.open()
+    homepage.click_monitor()
+    time.sleep(2)
+    homepage.check_products_count(2)
+
+    # driver.get('https://demoblaze.com/index.html')
+    # monitor_link = driver.find_element(By.CSS_SELECTOR, """[onclick="byCat('monitor')"]""")
+    # monitor_link.click()
+    # time.sleep(2)
+    # monitors = driver.find_elements(By.CSS_SELECTOR, '.card')
+    # assert len(monitors) == 2
+
+
